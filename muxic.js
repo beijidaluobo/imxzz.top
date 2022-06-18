ba34c922a2476c76e3fcbc8794cd95de68f3d62d
beijidaluobo <3313629407@qq.com>
2022-06-18 17:37:06 +0800

    up

--- muxic.js
+++ muxic.js
@@ -9,7 +9,7 @@ GitHub：https://github.com/imsyy/home
 */
 var server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "1052359111"; //封面 ID / 单曲 ID / 歌单 ID
+var id = "8516382042"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id, //json文件位置，文件名
