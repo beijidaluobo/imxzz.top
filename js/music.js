ff0ea3a30e98114dbff325d9f14fd0aa41cb1978
beijidaluobo <3313629407@qq.com>
2022-06-28 22:06:53 +0800

    up

--- js/music.js
+++ js/music.js
@@ -9,12 +9,12 @@ https://api.wuenci.com/meting/api/
 GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
-var server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
+var server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "7452421335"; //封面 ID / 单曲 ID / 歌单 ID
+var id = "8516382042"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
-    url: "https://api.wuen66666ci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id, //json文件位置，文件名
+    url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id, //json文件位置，文件名
     type: "GET",
     dataType: "JSON",
     success: function (data) {
